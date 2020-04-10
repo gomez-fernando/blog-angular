9a38c127452c50e83d801a00fac550924e7d4214
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-04-11 01:47:17 +0200

    creada ruta de página no encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// importar routing y appRoutingProviders
+import { routing, appRoutingProviders } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent, ErrorComponent],
+  // en imports se cargan los modulos
+  imports: [BrowserModule, routing],
+  // en providers se cargan los servicios
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css'],
+})
+export class ErrorComponent implements OnInit {
+  public pageTitle: string;
+
+  constructor() {
+    this.pageTitle = 'Página no encontrada';
+  }
+
+  ngOnInit(): void {}
+}
