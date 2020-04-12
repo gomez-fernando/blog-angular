d60bbeb4dc79192a275c561f350fcf86ede5351b
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-04-12 14:44:26 +0200

    listo mostrar datos del usuario en el front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { provideRoutes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // cargar UserService en providers
+  providers: [UserService],
 })
 export class AppComponent {
-  title = 'Blog Angular';
+  public title = 'Blog Angular';
+  public identity;
+  public token;
+
+  constructor(public userService: UserService) {
+    this.identity = this.userService.getIdentity();
+  }
 }
